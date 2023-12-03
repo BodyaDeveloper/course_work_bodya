0f86976f7f705f081af4d8b450d4d2f72a628500
Bohdan <bohdankovalchuk77@gmail.com>
2023-12-03 20:45:09 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+/*
+window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+});
+
+window.addEventListener("popstate", (event) => {
+    console.log(
+        `location: ${document.location}, state: ${JSON.stringify(event.state)}`,
+    );
+});*/
